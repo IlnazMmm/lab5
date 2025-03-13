aa63afb6dcdc8b2a7872a75d6c545f1a45c8b5d9
Мухаметзянов Ильназ <chizescrap@mail.ru>
2025-03-13 19:17:55 +0300

    halidov zdarova

--- part6-exercise2/ViewModels/MovieListViewModel.cs
+++ part6-exercise2/ViewModels/MovieListViewModel.cs
@@ -7,11 +7,22 @@ namespace MovieCatalog.ViewModels;
 public class MovieListViewModel: ObservableObject
 {
     private MovieViewModel? _selectedMovie;
-
+    public ICommand DeleteMovieCommand { get; private set; }
+    public MovieListViewModel()
+    {
+        Movies = [];
+        DeleteMovieCommand = new Command<MovieViewModel>(DeleteMovie);
+    }
     public MovieViewModel? SelectedMovie
     {
         get => _selectedMovie;
         set => SetProperty(ref _selectedMovie, value);
+    }
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        MenuItem menuItem = (MenuItem)sender;
+        ViewModels.MovieViewModel movie = (ViewModels.MovieViewModel)menuItem.BindingContext;
+        App.MainViewModel.DeleteMovie(movie);
     }
 
     public ObservableCollection<MovieViewModel> Movies { get; set; }
